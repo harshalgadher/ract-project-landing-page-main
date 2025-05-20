b003094489e82a0037b4b8189070c8365120efb1
789harshal <harshalgadher04@gmail.com>
2025-05-20 14:35:34 +0530

    responsive-header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,13 +8,15 @@ function Header() {
           <img src="https://avada.com/wp-content/uploads/2021/07/avada-logo-svg.svg" alt="logo" className="img-fluid" width={200} />
         </div>
 
-        <nav className="nav">
+        <div className="d-none d-lg-block">
+          <nav className="nav">
           <a href="#" className="nav-link">Avada ▾</a>
           <a href="#" className="nav-link">Avada For ▾</a>
           <a href="#" className="nav-link">Hosting</a>
           <a href="#" className="nav-link">Customization</a>
           <a href="#" className="nav-link">Resources ▾</a>
         </nav>
+        </div>
 
         <button className="button">✨ My Avada</button>
       </div>
